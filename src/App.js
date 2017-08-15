87e3328055194486fcf71c7d208b576ef54a9766
John Shea <sheajohnh@gmail.com>
2017-08-14 23:47:58 -0400

    Implement remove contact from database with ContactsAPI call

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends Component {
     this.setState((state) => ({
       contacts: state.contacts.filter((c) => c.id !== contact.id)
     }))
+    ContactsAPI.remove(contact)
   }
   render() {
     return (
